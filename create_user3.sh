ecef8d2b1eb4548d9efe8749f095c579f7d96c81
theurbanpenguin <andrew.mallett@theurbanpenguin.com>
2023-07-14 16:17:34 +0100

    Update and rename user3.sh to create_user3.sh

--- create_user3.sh
+++ create_user3.sh
@@ -7,7 +7,7 @@ else
 fi
 
 for username in "${usernames[@]}"; do
-  while grep -q "$username" /etc/passwd  ; do
+  while grep -q "^${username}:" /etc/passwd  ; do
     read -p "The '$username' user exists, please choose another name: " new_username
     username="$new_username"
   done
